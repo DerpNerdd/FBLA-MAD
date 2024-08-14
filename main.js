d4cdd7516b4422ea1613e9746f9a6050b6ba896c
Alan Sanchez <sanchez1.alan1@gmail.com>
2024-08-14 15:39:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-const { app, BrowserWindow } = require('electron');
-
-function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-
-    win.loadFile('index.html');
-}
-
-app.whenReady().then(() => {
-    createWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
-});
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef35d1c6fcb099b74b8150a153960dcf56cbdeccc62b608d51bbd450987218f2
+size 570
