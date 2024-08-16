dc0d7451c2b065f38fc72886bd7696750a9f07be
Alan Sanchez <sanchez1.alan1@gmail.com>
2024-08-16 14:44:52 -0700

    androidd

---
+++ android/app/src/main/java/derpnerdd/github/io/MainActivity.java
@@ -0,0 +1,5 @@
+package derpnerdd.github.io;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ www/resources/app/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ef35d1c6fcb099b74b8150a153960dcf56cbdeccc62b608d51bbd450987218f2
+size 570
