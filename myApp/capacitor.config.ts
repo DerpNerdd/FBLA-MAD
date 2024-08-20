5cb4d8a8c5d3aa0c2f2523604d7c84c03d96e231
DerpNerd <sanchez1.alan1@gmail.com>
2024-08-19 23:19:02 -0700

    PAIN
    ionic didnt work, switching to flutter tmrw

--- myApp/capacitor.config.ts
+++ myApp/capacitor.config.ts
@@ -1,9 +1,3 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myApp',
-  webDir: 'dist'
-};
-
-export default config;
+version https://git-lfs.github.com/spec/v1
+oid sha256:06d755cf9ec0922303e71d495d53a184ad926f9180c97775d127b037f6d40ad5
+size 153
