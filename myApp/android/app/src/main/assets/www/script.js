376f4c1007e0d0f409bfe131e72196968d5c372d
DerpNerd <sanchez1.alan1@gmail.com>
2024-08-28 15:16:28 -0700

    aaaaaaaaaaaaaaaaaaa

--- myApp/android/app/src/main/assets/www/script.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e53143b00d91a2b2407d0f95844d221aefc7ce5d5b3790450947b8a74117c305
-size 1729
--- myApp/capacitor.config.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:06d755cf9ec0922303e71d495d53a184ad926f9180c97775d127b037f6d40ad5
-size 153
